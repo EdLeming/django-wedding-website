0a86934d45528f80c66d000ad23dbab3226fcc04
EdLeming <e.leming09@googlemail.com>
2022-05-15 14:03:48 +0100

    Updated static files location

--- hannah-ed-wedding/settings.py
+++ hannah-ed-wedding/settings.py
@@ -121,11 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'static_root'
+#STATIC_ROOT = 'static_root'
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = (
+#    os.path.join('hannah-ed-wedding', 'static'),
+#)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join('hannah-ed-wedding', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
 
 # This is used in a few places where the names of the couple are used
 BRIDE_AND_GROOM = 'Hannah and Ed'
