81e0b09b6cce1562d04f8496028f5cb7f3b8e1d6
EdLeming <e.leming09@googlemail.com>
2022-05-19 22:33:35 +0100

    Update wedding_website_url

--- hannah-ed-wedding/settings.py
+++ hannah-ed-wedding/settings.py
@@ -184,7 +184,7 @@ DEFAULT_WEDDING_TEST_EMAIL = DEFAULT_WEDDING_FROM_EMAIL
 
 
 # This is used in links in save the date / invitations
-WEDDING_WEBSITE_URL = 'https://thehappycouple.com'
+WEDDING_WEBSITE_URL = 'https://hannah-ed-wedding.herokuapp.com/'
 WEDDING_CC_LIST = []  # put email addresses here if you want to cc someone on all your invitations
 
 # change to a real email backend in production
